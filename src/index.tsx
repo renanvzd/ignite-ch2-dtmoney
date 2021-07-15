a3a163592fb20020ee944df09e695385f1b4ebd3
Renan Veronez Drechsler <renanvz@outlook.com>
2021-07-15 18:47:58 -0300

    Configurando MirageJS

--- src/index.tsx
+++ src/index.tsx
@@ -1,11 +1,30 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import { App as Batata } from './App';
+import { createServer } from 'miragejs';
+import { App } from './App';
 
+createServer({
+  routes() {
+    this.namespace='api';
+
+    this.get('/transactions', () => {
+      return [
+        {
+          id: 1,
+          title: 'Transaction 1',
+          amount: 400,
+          type: 'deposit',
+          category: 'Food',
+          createdAt: new Date()
+        }
+      ]
+    })
+  }
+})
 
 ReactDOM.render(
   <React.StrictMode>
-    <Batata />
+    <App />
   </React.StrictMode>,
   document.getElementById('root')
 );
