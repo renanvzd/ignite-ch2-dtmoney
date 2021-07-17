83d4c674910562b697464a7ec84214f40db8dcd4
Renan Veronez Drechsler <renanvz@outlook.com>
2021-07-17 20:04:05 -0300

    Criando botoes de tipo

--- src/Components/NewTransactionModal/index.tsx
+++ src/Components/NewTransactionModal/index.tsx
@@ -1,5 +1,8 @@
 import Modal from 'react-modal';
-import { Container } from './styles';
+import incomeImg from '../../assets/income.svg';
+import outcomeImg from '../../assets/outcome.svg';
+import closeImg from '../../assets/close.svg';
+import { Container, TransactionTypeContainer } from './styles';
 
 Modal.setAppElement('#root');
 
@@ -16,9 +19,16 @@ export function NewTransactionModal({ isOpen, onRequestClose}: NewTransactionMod
     overlayClassName="react-modal-overlay"
     className="react-modal-content"
   >
+    <button 
+      type="button" 
+      onClick={onRequestClose} 
+      className="react-modal-close">
+
+      <img src={closeImg} alt="Fechar modal" />
+    </button>
+  
    <Container>
     <h2>Cadastrar transação</h2>
-
     <input
       placeholder="Título"
     />
@@ -26,6 +36,20 @@ export function NewTransactionModal({ isOpen, onRequestClose}: NewTransactionMod
       type="number"
       placeholder="Valor"
     />
+
+    <TransactionTypeContainer>
+      <button 
+      type="button">
+        <img src={incomeImg} alt="Entrada" />
+        <span>Entrada</span>
+      </button>
+      <button 
+      type="button">
+        <img src={outcomeImg} alt="Saída" />
+        <span>Saída</span>
+      </button>
+    </TransactionTypeContainer>
+
     <input
       placeholder="Categoria"
     />
